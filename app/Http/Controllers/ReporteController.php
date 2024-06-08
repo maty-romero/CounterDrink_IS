60baae332f69aefff8415a02f94d68843189018e
Hans <ich.arbeite.rd>
2024-06-08 17:07:20 -0300

    fix redirect al loggearse

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -14,51 +14,6 @@ class ReporteController extends Controller
         return view('administrativa.reportes.index');
     }
 
-    /*
-    public function comprobanteVenta(string $comprobante)
-    {
-        $ventaCheck = Venta::where("id", $comprobante)->get(); 
-
-        if (count($ventaCheck) === 0) {
-            return abort(404);
-        }
-        $jasper = new PHPJasper();
-
-        $path_subreport = base_path() . '/database/reportes/';
-        $input = base_path() . '\database\reportes\FacturaCliente.jrxml';
-        $output = base_path() . '\database\reportes\output\FacturaCliente';
-
-        $cliente = session()->get('cliente');
-        $nombre_cliente = $cliente->nombre_cliente; 
-        $dni = $cliente->dni; 
-
-        $options = [
-            'format' => ['pdf'],
-            'locale' => 'en',
-            'params' => [
-                'NroVenta' => $comprobante,
-                'nombre_cliente' => $nombre_cliente,
-                'dni' => $dni,
-                "SubReportPath" => $path_subreport
-            ],
-            'db_connection' => [
-                //datos conexión base
-                'driver' => 'mysql',
-                'host' => '127.0.0.1',
-                'port' => '3306',
-                'database' => 'counterdrink',
-                'username' => 'root'
-            ],
-        ];
-
-        $jasper->compile($input)->execute();
-        $jasper->process($input, $output, $options)->execute();
-        $pathToFile = base_path() . '\database\reportes\output\FacturaCliente.pdf';
-
-        return response()->file($pathToFile);
-    }
-    */
-
     public function comprobanteVenta(string $comprobante)
     {
         $ventaCheck = Venta::where("id", $comprobante)->get();
