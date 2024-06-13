1f0bc34a81e2a8d3817c8c62b9e80beee0647473
Hans <ich.arbeite.rd>
2024-06-13 18:03:01 -0300

    update routes y navbar segun rol

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -3,14 +3,27 @@
         <div class="sb-sidenav-menu">
             <div class="nav">
                 <div class="sb-sidenav-menu-heading">Gestion</div>
-                <a class="nav-link"  href="{{ route('usuarios_index') }}">
-                    <div class="sb-nav-link-icon"><i class="fa fa-user"></i></div>
-                    Usuarios
-                </a>
-                <a class="nav-link" href="{{ route('proveedores_index') }}">
-                    <div class="sb-nav-link-icon"><i class="fa fa-truck"></i></div>
-                    Proveedores
-                </a>
+
+                @php
+                    $esAdministrador = Auth::user()->rol_usuario == 'administrador'; 
+                    $esAbastecedor = Auth::user()->rol_usuario == 'abastecedor';
+                    $esCajero = Auth::user()->rol_usuario == 'cajero';
+                @endphp
+
+                @if ($esAdministrador)
+                    <a class="nav-link"  href="{{ route('usuarios_index') }}">
+                        <div class="sb-nav-link-icon"><i class="fa fa-user"></i></div>
+                        Usuarios
+                    </a>    
+                @endif
+                
+                @if ($esAdministrador || $esAbastecedor)
+                    <a class="nav-link" href="{{ route('proveedores_index') }}">
+                        <div class="sb-nav-link-icon"><i class="fa fa-truck"></i></div>
+                        Proveedores
+                    </a>    
+                @endif
+                
                 <a class="nav-link" href="{{ route('productos_index') }}">
                     <div class="sb-nav-link-icon"><i class="fa fa-glass"></i></div>
                     Productos
@@ -19,14 +32,21 @@
                     <div class="sb-nav-link-icon"><i class="fa fa-credit-card-alt"></i></div>
                     Ventas
                 </a>
-                <a class="nav-link" href="{{ route('stock_index') }}">
-                    <div class="sb-nav-link-icon"><i class="fa fa-list-ol"></i></div>
-                    Stock
-                </a>
-                <a class="nav-link" href="{{ route('reportes_index') }}">
-                    <div class="sb-nav-link-icon"><i class="fa fa-info"></i></div>
-                    Reportes
-                </a>
+
+                @if ($esAdministrador || $esAbastecedor)
+                    <a class="nav-link" href="{{ route('stock_index') }}">
+                        <div class="sb-nav-link-icon"><i class="fa fa-list-ol"></i></div>
+                        Stock
+                    </a>
+                @endif
+                
+                @if ($esAdministrador || $esAbastecedor)
+                    <a class="nav-link" href="{{ route('reportes_index') }}">
+                        <div class="sb-nav-link-icon"><i class="fa fa-info"></i></div>
+                        Reportes
+                    </a>
+                @endif
+                
             </div>
         </div>
     </nav>
--- routes/web.php
+++ routes/web.php
@@ -9,24 +9,15 @@ use App\Http\Controllers\ReporteController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-// Rutas anteriores 
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-/*
-Route::get('/', function () {
-    return view('/administrativa/productos/Stock');
-})->name('holamundo');
-*/ 
-
 Auth::routes();
 
-// Rutas Client side
+// ** Rutas Client side
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
     ->name('home');  // No muy necesario -> Ver flujo de autenticacion
 
 Route::get('/', [VentaController::class, 'homeShop'])->name('home_shop');
 Route::get('/show/{id}', [ProductoController::class, 'show'])->name('show_product');
-// Rutas carrito Client side
+
 Route::get('/carrito', [VentaController::class, 'showCart'])->name('show_carrito');
 Route::post('/carrito/{id}', [VentaController::class, 'updateCart'])->name('carrito_agregar')->middleware('web');
 Route::patch('/carrito/{id}', [VentaController::class, 'editCart'])->name('carrito_editar');
@@ -35,54 +26,109 @@ Route::delete('/carrito/{id}', [VentaController::class, 'removeFromCart'])->name
 Route::post('/ventas/store/online', [VentaController::class, 'storeVentaOnline'])->name('venta_online_finalizar');
 Route::get('/reportes/comprobanteVenta/{nroComprobante}',  [ReporteController::class, "comprobanteVenta"])->name('cliente_comprobante_venta');
 
-
-// Rutas Administration side
-Route::get('/usuarios', [UsuarioController::class, 'index'])->name('usuarios_index');
-Route::get('/usuarios/edit/{id}', [UsuarioController::class, 'edit'])->name('usuarios_edit');
-Route::put('/usuarios/update/{id}', [UsuarioController::class, 'update'])->name('usuarios_update');
-Route::get('/usuarios/create', [UsuarioController::class, 'create'])->name('usuarios_create');
-Route::post('/usuarios/store', [UsuarioController::class, 'store'])->name('usuarios_store');
-Route::delete('/usuarios/destroy/{id}', [UsuarioController::class, 'destroy'])->name('usuarios_destroy');
-
-
-Route::get('/ventas', [VentaController::class, 'index'])->name('ventas_index');
-Route::get('/ventas/create', [VentaController::class, 'create'])->name('ventas_create');
-Route::post('/ventas/store/presencial', [VentaController::class, 'storeVentaPresencial'])->name('venta_presencial_finalizar');
-Route::post('/existeVentas', [VentaController::class, 'validarVentas'])->name('validarVentas');
-// Registro nueva venta  
-Route::post('/ventas/agregar/{id}', [VentaController::class, 'agregarProductoDetalleVenta'])->name('ventas_agregar_producto')->middleware('web');
-Route::post('/ventas/actualizar/{id}', [VentaController::class, 'actualizarCantidadProductoDetalleVenta'])->name('ventas_actualizar_producto');
-Route::post('/ventas/eliminar/{id}', [VentaController::class, 'eliminarProductoDetalleVenta'])->name('ventas_eliminar_producto')->middleware('web');
-Route::post('/ventas/store/presencial', [VentaController::class, 'storeVentaPresencial'])->name('venta_presencial_finalizar');
-
-
-
-
-Route::get('/proveedores', [ProveedorController::class, 'index'])->name('proveedores_index');
-Route::get('/proveedores/edit/{id}', [ProveedorController::class, 'edit'])->name('proveedores_edit');
-Route::get('/proveedores/create', [ProveedorController::class, 'create'])->name('proveedores_create');
-
-Route::post('/proveedores/store', [ProveedorController::class, 'store'])->name('proveedores_store');
-Route::put('/proveedores/update/{id}', [ProveedorController::class, 'update'])->name('proveedores_update')->middleware('web');
-Route::delete('/proveedores/delete/{id}', [ProveedorController::class, 'destroy'])->name('proveedores_destroy')->middleware('web');
-
-
-Route::get('/productos', [ProductoController::class, 'index'])->name('productos_index');
-Route::get('/productos/edit/{id}', [ProductoController::class, 'edit'])->name('productos_edit');
-Route::put('/productos/update/{id}', [ProductoController::class, 'update'])->name('producto_update');
-Route::get('/productos/create', [ProductoController::class, 'create'])->name('productos_create');
-Route::post('/productos/store', [ProductoController::class, 'store'])->name('productos_store');
-Route::delete('/productos/destroy/{id}', [ProductoController::class, 'destroy'])->name('productos_destroy');
-Route::get('/productos/show/{id}', [ProductoController::class, 'show'])->name('productos_show');
-
-Route::get('/productos/stock', [StockController::class, 'index'])->name('stock_index');
 Route::get('/productos/search/cliente', [ProductoController::class, 'clientSearch'])->name('productos_client_search');
 
 
-Route::get('/reportes', [ReporteController::class, 'index'])->name('reportes_index');
-Route::post("/reporteRedirect", [ReporteController::class, "reporteRedirect"])->name("reporteRedirect");
+// ** Rutas Roles Administrativos 
 
-Route::post('/productos/stock', [StockController::class, 'update'])->name('stock_update');
+Route::middleware(['auth'])->group(function(){
+    Route::get('/usuarios', [UsuarioController::class, 'index'])->name('usuarios_index');
+    Route::get('/usuarios/edit/{id}', [UsuarioController::class, 'edit'])->name('usuarios_edit');
+    Route::put('/usuarios/update/{id}', [UsuarioController::class, 'update'])->name('usuarios_update');
+    Route::get('/usuarios/create', [UsuarioController::class, 'create'])->name('usuarios_create');
+    Route::post('/usuarios/store', [UsuarioController::class, 'store'])->name('usuarios_store');
+    Route::delete('/usuarios/destroy/{id}', [UsuarioController::class, 'destroy'])->name('usuarios_destroy');
+    
+    Route::get('/ventas', [VentaController::class, 'index'])->name('ventas_index');
+    Route::get('/ventas/create', [VentaController::class, 'create'])->name('ventas_create');
+    Route::post('/ventas/store/presencial', [VentaController::class, 'storeVentaPresencial'])->name('venta_presencial_finalizar');
+    Route::post('/existeVentas', [VentaController::class, 'validarVentas'])->name('validarVentas');
+    // Registro nueva venta presencial
+    Route::post('/ventas/agregar/{id}', [VentaController::class, 'agregarProductoDetalleVenta'])->name('ventas_agregar_producto')->middleware('web');
+    Route::post('/ventas/actualizar/{id}', [VentaController::class, 'actualizarCantidadProductoDetalleVenta'])->name('ventas_actualizar_producto');
+    Route::post('/ventas/eliminar/{id}', [VentaController::class, 'eliminarProductoDetalleVenta'])->name('ventas_eliminar_producto')->middleware('web');
+    Route::post('/ventas/store/presencial', [VentaController::class, 'storeVentaPresencial'])->name('venta_presencial_finalizar');
+    
+    Route::get('/proveedores', [ProveedorController::class, 'index'])->name('proveedores_index');
+    Route::get('/proveedores/edit/{id}', [ProveedorController::class, 'edit'])->name('proveedores_edit');
+    Route::get('/proveedores/create', [ProveedorController::class, 'create'])->name('proveedores_create');
+    Route::post('/proveedores/store', [ProveedorController::class, 'store'])->name('proveedores_store');
+    Route::put('/proveedores/update/{id}', [ProveedorController::class, 'update'])->name('proveedores_update')->middleware('web');
+    Route::delete('/proveedores/delete/{id}', [ProveedorController::class, 'destroy'])->name('proveedores_destroy')->middleware('web');
+
+    Route::get('/productos', [ProductoController::class, 'index'])->name('productos_index');
+    Route::get('/productos/edit/{id}', [ProductoController::class, 'edit'])->name('productos_edit');
+    Route::put('/productos/update/{id}', [ProductoController::class, 'update'])->name('producto_update');
+    Route::get('/productos/create', [ProductoController::class, 'create'])->name('productos_create');
+    Route::post('/productos/store', [ProductoController::class, 'store'])->name('productos_store');
+    Route::delete('/productos/destroy/{id}', [ProductoController::class, 'destroy'])->name('productos_destroy');
+    Route::get('/productos/show/{id}', [ProductoController::class, 'show'])->name('productos_show');
+
+    Route::get('/productos/stock', [StockController::class, 'index'])->name('stock_index');
+    Route::post('/productos/stock', [StockController::class, 'update'])->name('stock_update');
+
+    Route::get('/reportes', [ReporteController::class, 'index'])->name('reportes_index');
+    Route::post("/reporteRedirect", [ReporteController::class, "reporteRedirect"])->name("reporteRedirect");
+    
+}); 
+
+
+// Rutas permitidas para todos los roles 
+/*
+Route::group(['middleware' => ['auth', 'checkrole:cajero,abastecedor,administrador']], function() {
+    Route::get('/ventas', [VentaController::class, 'index'])->name('ventas_index');
+    Route::get('/productos', [ProductoController::class, 'index'])->name('productos_index');
+    Route::post('/existeVentas', [VentaController::class, 'validarVentas'])->name('validarVentas');
+});
+
+Route::group(['middleware' => ['auth', 'checkrole:administrador']], function() {
+    Route::get('/usuarios', [UsuarioController::class, 'index'])->name('usuarios_index');
+    Route::get('/usuarios/edit/{id}', [UsuarioController::class, 'edit'])->name('usuarios_edit');
+    Route::put('/usuarios/update/{id}', [UsuarioController::class, 'update'])->name('usuarios_update');
+    Route::get('/usuarios/create', [UsuarioController::class, 'create'])->name('usuarios_create');
+    Route::post('/usuarios/store', [UsuarioController::class, 'store'])->name('usuarios_store');
+    Route::delete('/usuarios/destroy/{id}', [UsuarioController::class, 'destroy'])->name('usuarios_destroy');    
+});
+
+Route::group(['middleware' => ['auth', 'checkrole:abastecedor']], function() {
+    Route::get('/productos/edit/{id}', [ProductoController::class, 'edit'])->name('productos_edit');
+    Route::put('/productos/update/{id}', [ProductoController::class, 'update'])->name('producto_update');
+    Route::get('/productos/create', [ProductoController::class, 'create'])->name('productos_create');
+    Route::post('/productos/store', [ProductoController::class, 'store'])->name('productos_store');
+    Route::delete('/productos/destroy/{id}', [ProductoController::class, 'destroy'])->name('productos_destroy');
+    Route::get('/productos/show/{id}', [ProductoController::class, 'show'])->name('productos_show');
+
+    Route::get('/productos/stock', [StockController::class, 'index'])->name('stock_index');
+    Route::post('/productos/stock', [StockController::class, 'update'])->name('stock_update');
+
+    Route::get('/proveedores', [ProveedorController::class, 'index'])->name('proveedores_index');
+    Route::get('/proveedores/edit/{id}', [ProveedorController::class, 'edit'])->name('proveedores_edit');
+    Route::get('/proveedores/create', [ProveedorController::class, 'create'])->name('proveedores_create');
+    Route::post('/proveedores/store', [ProveedorController::class, 'store'])->name('proveedores_store');
+    Route::put('/proveedores/update/{id}', [ProveedorController::class, 'update'])->name('proveedores_update')->middleware('web');
+    Route::delete('/proveedores/delete/{id}', [ProveedorController::class, 'destroy'])->name('proveedores_destroy')->middleware('web');
+
+    Route::get('/reportes', [ReporteController::class, 'index'])->name('reportes_index');
+    Route::post("/reporteRedirect", [ReporteController::class, "reporteRedirect"])->name("reporteRedirect");       
+});
+
+Route::group(['middleware' => ['auth', 'checkrole:cajero']], function() {
+    Route::get('/ventas/create', [VentaController::class, 'create'])->name('ventas_create');
+    Route::post('/ventas/store/presencial', [VentaController::class, 'storeVentaPresencial'])->name('venta_presencial_finalizar');
+    // Registro nueva venta presencial
+    Route::post('/ventas/agregar/{id}', [VentaController::class, 'agregarProductoDetalleVenta'])->name('ventas_agregar_producto')->middleware('web');
+    Route::post('/ventas/actualizar/{id}', [VentaController::class, 'actualizarCantidadProductoDetalleVenta'])->name('ventas_actualizar_producto');
+    Route::post('/ventas/eliminar/{id}', [VentaController::class, 'eliminarProductoDetalleVenta'])->name('ventas_eliminar_producto')->middleware('web');
+    Route::post('/ventas/store/presencial', [VentaController::class, 'storeVentaPresencial'])->name('venta_presencial_finalizar');        
+});
+
+
+Route::get('/unauthorized', function () {
+    return "No estas autorizado! AFUERA!";
+})->name('unauthorized');
+
+*/
+
 
 
 
